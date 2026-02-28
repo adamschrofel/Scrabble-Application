cbc69d5886ec10d766ba6ab467f5700367f1f7fe
AdamS <aschrofel10@gmail.com>
2026-02-28 15:24:58 -0600

    chore(ci): added speedInsights from vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    <Analytics/>
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 );
